9394bba3b224ee37115696c280e06188c1951fdc
william-lundgren <superbanan@live.se>
2023-01-03 16:02:05 +0100

    Added exclusion for for empty day and correct time for scheduler

--- classroom-mail.py
+++ classroom-mail.py
@@ -1,16 +1,23 @@
 import requests
 from bs4 import BeautifulSoup as bs
-from datetime import datetime
 import smtplib
 import ssl
 from keep_alive import keep_alive
 from os import getenv
 import schedule
 import time
+from datetime import datetime
+
+
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 
 def send_mail(receiver_email, content, day):
-    port = 465
+    # Setup and send mail
+    port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     sender_email = getenv("sender")
     password = getenv("password")
@@ -20,7 +27,10 @@ Subject: Dagens bra klassrum ({day}). Ha en fin dag.
 {content}"""
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        print("Logging in!", get_time())
         server.login(sender_email, password)
+
+        print("Sending mail", get_time())
         server.sendmail(sender_email, receiver_email, message)
 
 
@@ -34,7 +44,7 @@ def add(key, dic, count):
         dic[key] = count
 
 
-def scrape(url, day):  # Make day preset to current date
+def scrape(url, day):
     # Setup html and get response
     response = requests.get(url)
     html = response.content
@@ -60,20 +70,11 @@ def scrape(url, day):  # Make day preset to current date
     no_of_bookings = {}
 
     # All possible classrooms
-    classrooms = ["MH:227",
-                  "MH:228",
-                  "MH:229",
-                  "MH:309A",
-                  "MH:309B",
-                  "MH:309C",
-                  "MH:331",
-                  "MH:332A",
-                  "MH:332B",
-                  "MH:333",
-                  "MH:362A",
-                  "MH:362B",
-                  "MH:362C",
-                  "MH:362D"]
+    classrooms = [
+        "MH:227", "MH:228", "MH:229", "MH:309A", "MH:309B", "MH:309C",
+        "MH:331", "MH:332A", "MH:332B", "MH:333", "MH:362A", "MH:362B",
+        "MH:362C", "MH:362D"
+    ]
 
     # Set all possible values, maybe make it customizable for more classrooms
     for classroom in classrooms:
@@ -92,17 +93,20 @@ def scrape(url, day):  # Make day preset to current date
             times_booked[ele] = times_booked[ele][1:]
 
     print(f"Current day: {day}")
-#    for i in times_booked:
-#        print(i, times_booked.get(i))
     sorted_list = sorted(no_of_bookings.items(), key=lambda x: x[1])
 
+    # Large string of format {no of bookings} {classroom} {times it is booked} sorted by no of bookings
     sorted_string = ""
     for i in sorted_list:
         sorted_string += f"{i[1]} {i[0]} {times_booked.get(i[0])}\n"
+        # print(i[0], times_booked.get(i[0]))
+
+    # Get rid of final newline character
+    sorted_string = sorted_string[:-1]
 
     # Find all the lowest values
-    min_val = min(no_of_bookings.values())
-    #res = [key for key, value in no_of_bookings.items() if value == min_val]
+    # min_val = min(no_of_bookings.values())
+    # res = [key for key, value in no_of_bookings.items() if value == min_val]
 
     return sorted_string
 
@@ -115,27 +119,43 @@ use find(bookingDiv) (class) on every day from classrooms
 '''
 
 
+def empty(day):
+    # Check for leading 0s, if all are then its empty
+    print(day.split("\n")[-1])
+    for classroom in day.split("\n"):
+        if classroom[0] != "0":
+            return False
+    return True
+
+
 def setup():
-    # LP 2
+    # Schedule url for every classroom
     url = "https://cloud.timeedit.net/lu/web/lth1/ri16W65057506XQQ55ZZ767005yYW64856Y05506Q65116X65262902465144XY70016755X0Y5Y247X1052252X5176456060X49Y54457Y6557104X670Y5822Y1450862X46Y47X6095X72551501Y40Y014XX562424027Y546165X36743997162730X504YY5X4625676047QY.html"
-    # Level 3 and 2
-    # url = "https://cloud.timeedit.net/lu/web/lth1/ri16666565000YQQ65Z652500Xy7Y4810g75g0X6Y65ZW6465X2Q10X126004Y745502461Y5767X54Y055X06XY5042952511Y6476X8647455205XY245761X09075522XY82971Y4545Y4X6615507257904YY63X1141X04632025YX64516406674X5Y702746YX30Q20456Y794756.html"
-    # For all level 3
-    # url = "https://cloud.timeedit.net/lu/web/lth1/ri16666565000YQQ65Z652500Xy7Y4810g75g0X6Y65ZW6465X2Q10X126004Y745502461Y5767X54Y055X06XY5042952511Y6476X8647455205XY245761X09075522XY82971Y4545Y4X66155072Y70445Y6251160X7Q7.html"
-    ''
-#    day = datetime.today().strftime('%Y-%m-%d')
-    day = "2022-12-13"
+
+    # day of format YYYY-MM-DD
+    day = datetime.today().strftime('%Y-%m-%d')
+
     res = scrape(url, day)
 
-    for mail in getenv("password").split(","):
-        send_mail(mail, res, day)
+    # Only send mail if there are things scheduled
+    if not empty(res):
+        for mail in getenv("mails").split(","):
+            send_mail(mail, res, day)
+    else:
+        print("Empty day:", day)
 
 
 def main():
     keep_alive()
 
-    #schedule.every(3).minutes.do(setup)
-    schedule.every().day.at("07:30").do(setup)
+    # Entered time is not always same as time program think because of time zones, a lazy fix but it works for this purpose.
+    wanted_time = "07:30"
+
+    # Subtract one from first 2 digits, keeping day change and leading 0s in mind
+    formatted = (int(wanted_time.split(":")[0]) - 1) % 24
+    correct_time = f"{formatted:02}{wanted_time[2:]}"
+
+    schedule.every().day.at(correct_time).do(setup)
 
     while True:
         schedule.run_pending()
